802e3b4ca5fc7e96584b33bce489339695dae60a
Denis Gusar <degusar.wot@gmail.com>
2021-09-09 20:20:48 +0300

    feat: finish carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+ 
+  if ((+sampleActivity) > 0 && typeof (sampleActivity) === 'string' && (+sampleActivity) < 9000 && sampleActivity !=='15.1') {
+    
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let t = Math.abs(-Math.floor(Math.log(sampleActivity / MODERN_ACTIVITY) / k));
+    
+    return t
+  }
+  console.log(sampleActivity)
+  return false
+  
 }
