b939bcc7fa847c4e2cf6f058b7c55b143bd4fca3
Denis Gusar <degusar.wot@gmail.com>
2021-10-04 21:13:05 +0300

    feat: finish task extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,41 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  str = String(str);
+  
+  let bonusPart = '';
+  let addition = (options.addition !== undefined) ? String(options.addition) : '';
+  let separator = (!!options.separator) ? options.separator : '+';
+  let additionSeparator = (options.additionSeparator !== undefined) ? options.additionSeparator : '|';
+  let result = '';
+  let i = 0;
+
+  do {
+     if ((options.addition !== undefined)&&options.additionRepeatTimes> 1) {
+     bonusPart = bonusPart + addition + additionSeparator;
+     
+    } else  {
+      bonusPart = addition ;
+    }
+    i++
+  } while (i < options.additionRepeatTimes - 1);
+  
+  if ((options.addition !== undefined)&&options.additionRepeatTimes > 1) {
+    bonusPart = bonusPart + addition
+  }
+   
+  for (let i = 0; i < options.repeatTimes - 1; i++) {
+    if (options.addition !== undefined) {
+      result = result + str + bonusPart + separator;
+    } else {
+      result = result + str + separator;
+    }
+    
+  };
+
+  return result = result + str + bonusPart;
+  
+  
 }
+/* npm run test -- test/extended-repeater.test.js */
